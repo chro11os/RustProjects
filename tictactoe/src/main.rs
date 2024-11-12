13e6d6340f6a278dc65ed896c26f9bbbf0f2421d
chrollos <106138109+chro11os@users.noreply.github.com>
2024-11-13 02:36:15 +0800

    making tictactoe from c++ to rust part 1

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+fn drawBoard(board: &[[char; 3]; 3]) {
+    let i:i32 = 3;
+    let j:i32 = 3;
+
+    println!("-------------");
+
+    for i in i {
+        println!("| ");
+        for j in j {
+            drawBoard(board[i:usize][j:usize]);
+        }
+    }
+    println!("-------------");
 }
+fn main() {}
