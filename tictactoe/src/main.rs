dfb02baff98ef4100ffb245602e13e6123a7b427
chrollos <106138109+chro11os@users.noreply.github.com>
2024-11-17 02:02:41 +0800

    fixing code in the loop

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -1,17 +1,19 @@
 use std::io;
 fn drawBoard(board: &[[char; 3]; 3]) {
-    let mut range = 1..3;
+    let mut BoardRange = 1..3;
 
     println!("-------------");
 
-    for i in range {
+    for i in BoardRange {
         println!("| ");
-        for j in range {
-            drawBoard(board[i:usize][j:usize]);
+        for j in BoardRange {
+            println!("{}", board[i][j]);
         }
     }
+
     println!("-------------");
 }
+
 fn main() {
-    drawBoard(&[]);
+    drawBoard();
 }
