9155c4719d964d73815b09e9ac155cc740750be1
chrollos <106138109+chro11os@users.noreply.github.com>
2024-11-28 16:12:44 +0800

    change .clones to use borrowing

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -1,18 +1,27 @@
-use std::{io, ops::Range};
+use std::{ops::Range};
 
 fn drawBoard(board: &[[char; 3]; 3]) {
-    let mut BoardRange: Range<usize> = 1..3;
-
+    let BoardRange: Range<usize> = 1..3;
+    
     println!("-------------");
-
-    for i in BoardRange.clone() {
-        println!("| ");
-        for j in BoardRange.clone() {
+    for i in &BoardRange {
+        println!("| ", );
+        for j in &BoardRange {
             println!("{}", board[i][j]);
         }
     }
-
     println!("-------------");
+}
+fn checkWin(board: &[[char; 3]; 3], player: char) -> bool {
+    let PlayerRange: Range<usize> = 1..3;
+    if board[&PlayerRange][0] == player
+        && board[&PlayerRange][1] == player
+        && board[&PlayerRange][2] == player
+    {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 fn main() {}
